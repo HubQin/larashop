b1cf4db04911c6fe5965da126fd3025662552050
hubqin <wuhanqinb@gmail.com>
2020-02-12 14:03:53 +0000

    create address

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,8 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
