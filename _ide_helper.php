0fe6089c7da4807776e9beafcefc57aa67e4c5e4
hubqin <wuhanqinb@gmail.com>
2020-02-15 12:36:09 +0000

    admin:new and edit product

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.14.0 on 2020-02-12 15:18:50.
+ * Generated for Laravel 6.14.0 on 2020-02-15 12:14:08.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
