9c99e15142d358c12da6f5535b85515dcfed4810
hubqin <wuhanqinb@gmail.com>
2020-07-25 11:19:55 +0800

    add create product index to elasticsearch

--- app/Admin/Controllers/CommonProductsController.php
+++ app/Admin/Controllers/CommonProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Jobs\SyncOneProductToEs;
 use Encore\Admin\Controllers\AdminController;
 use App\Models\Product;
 use Encore\Admin\Form;
@@ -70,6 +71,11 @@ abstract class CommonProductsController extends AdminController
         });
         $form->saving(function (Form $form) {
             $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+        });
+
+        $form->saved(function (Form $form){
+            $product = $form->model();
+            dispatch(new SyncOneProductToEs($product));
         });
 
         return $form;
