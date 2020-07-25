0941a0ca57e319f9c1045114567c871c12faac40
hubqin <wuhanqinb@gmail.com>
2020-07-25 14:08:17 +0800

    search by es

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,77 +3,67 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
-use Illuminate\Http\Request;
-use App\Models\Product;
 use App\Models\OrderItem;
-use App\Models\Category;
-use App\Services\CategoryService;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProductsController extends Controller
 {
-    public function index(Request $request)
-    {
-        // 创建一个查询构造器
-        $builder = Product::query()->where('on_sale', true);
-        // 判断是否有提交 search 参数，如果有就赋值给 $search 变量
-        // search 参数用来模糊搜索商品
-        if ($search = $request->input('search', '')) {
-            $like = '%'.$search.'%';
-            // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
-            $builder->where(function ($query) use ($like) {
-                $query->where('title', 'like', $like)
-                    ->orWhere('description', 'like', $like)
-                    ->orWhereHas('skus', function ($query) use ($like) {
-                        $query->where('title', 'like', $like)
-                            ->orWhere('description', 'like', $like);
-                    });
-            });
-        }
+    public function index(Request $request){
+        $page    = $request->input('page', 1);
+        $perPage = 16;
 
-        // 如果有传入 category_id 字段，并且在数据库中有对应的类目
-        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
-            // 如果这是一个父类目
-            if ($category->is_directory) {
-                // 则筛选出该父类目下所有子类目的商品
-                $builder->whereHas('category', function ($query) use ($category) {
-                    // 这里的逻辑参考本章第一节
-                    $query->where('path', 'like', $category->path.$category->id.'-%');
-                });
-            } else {
-                // 如果这不是一个父类目，则直接筛选此类目下的商品
-                $builder->where('category_id', $category->id);
-            }
-        }
+        $params = [
+            'index' => 'products',
+            'body'  => [
+                'from'  => ($page - 1) * $perPage,
+                'size'  => $perPage,
+                'query' => [
+                    'bool' => [
+                        'filter' => [
+                            ['term' => ['on_sale' => true]],
+                        ],
+                    ],
+                ],
+            ],
+        ];
 
         // 是否有提交 order 参数，如果有就赋值给 $order 变量
         // order 参数用来控制商品的排序规则
-        if ($order = $request->input('order', '')) {
+        if($order = $request->input('order', '')) {
             // 是否是以 _asc 或者 _desc 结尾
-            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+            if(preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
                 // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
-                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                if(in_array($m[1], ['price', 'sold_count', 'rating'])) {
                     // 根据传入的排序值来构造排序参数
-                    $builder->orderBy($m[1], $m[2]);
+                    $params['body']['sort'] = [[$m[1] => $m[2]]];
                 }
             }
         }
 
-        $products = $builder->paginate(16);
+        $result     = app('es')->search($params);
+        $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        $products   = Product::query()
+            ->whereIn('id', $productIds)
+            ->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $productIds)))
+            ->get();
+        $pager      = new LengthAwarePaginator($products, $result['hits']['total']['value'], $perPage, $page, ['path' => route('products.index', false)]);
+
 
         return view('products.index', [
-            'products' => $products,
+            'products' => $pager,
             'filters'  => [
-                'search' => $search,
+                'search' => '',
                 'order'  => $order,
             ],
-            'category' => $category ?? null,
+            'category' => null,
         ]);
     }
 
-    public function show(Product $product, Request $request)
-    {
+    public function show(Product $product, Request $request){
         // 判断商品是否已经上架，如果没有上架则抛出异常。
-        if (!$product->on_sale) {
+        if(!$product->on_sale) {
             throw new InvalidRequestException('商品未上架');
         }
 
@@ -97,10 +87,9 @@ class ProductsController extends Controller
         return view('products.show', ['product' => $product, 'favored' => $favored, 'reviews' => $reviews]);
     }
 
-    public function favor(Product $product, Request $request)
-    {
+    public function favor(Product $product, Request $request){
         $user = $request->user();
-        if ($user->favoriteProducts()->find($product->id)) {
+        if($user->favoriteProducts()->find($product->id)) {
             return [];
         }
 
@@ -109,16 +98,14 @@ class ProductsController extends Controller
         return [];
     }
 
-    public function disfavor(Product $product, Request $request)
-    {
+    public function disfavor(Product $product, Request $request){
         $user = $request->user();
         $user->favoriteProducts()->detach($product);
 
         return [];
     }
 
-    public function favorites(Request $request)
-    {
+    public function favorites(Request $request){
         $products = $request->user()->favoriteProducts()->paginate(16);
 
         return view('products.favorites', ['products' => $products]);
